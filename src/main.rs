004f7683924fd68c0053494420ced42e03b17b2c
Aryan Tamang <aryantmg628@gmail.com>
2024-11-08 22:50:47 +0545

    feat : minigrep logic is sperated in lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use minigrep::Config;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config: Result<Config, &str> = Config::build(&args);
+
+    match config {
+        Ok(config) => {
+            if let Err(err) = Config::run(config) {
+                println!("--------------------{}-------------------------", err);
+                process::exit(1);
+            }
+        }
+        Err(_err) => {
+            println!("--------------------Invalid Arguments-------------------------");
+            println!("Expected, cargo run -- <qeurystring> <filepath>  ");
+            println!("--------------------------------------------------------------");
+            process::exit(1);
+        }
+    }
 }
